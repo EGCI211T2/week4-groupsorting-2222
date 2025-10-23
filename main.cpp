79f98a80039e1e58044317332ec9da0aa211ce2b
punyisa-6780631 <punyisa.pan@student.mahidol.edu>
2025-10-23 08:21:43 +0000

    pp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,28 @@
 using namespace std;
 #include "sorting.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv) {
   int i, j, n = argc-2;
   int *a;
 
-   string sortType = argv[1];
-  a = new int[n];
+  string sortType = argv[1];
+  a = new int[n];//pointer
 
   for(i = 0; i<n; i++){
     a[i] = atoi(argv[i+2]);
   }
 
+  cout<<"Before: ";
   display(a,n);
-
-  if(sortType == "insert"){
-    insertion(a,n); 
-  }
+  if(sortType=="insert") insertSort(a,n);
+  else if(sortType=="bubble") bubbleSort(a,n);
+  else if(sortType=="select") selectSort(a,n);
+  else if(sortType=="heap") heapSort(a,n);
 
   display(a,n);
 
   delete[]a;
- return 0; 
+  return 0; 
 }
 
 
