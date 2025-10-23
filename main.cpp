23bc8e7de2e431733c21caadaded76f8d10d5b02
punyisa-6780631 <punyisa.pan@student.mahidol.edu>
2025-10-23 10:42:32 +0000

    mm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+/*#include <algorithm>
+#include <chrono>
+
+#include<vector>
+
+using namespace std::chrono;*/
 
 using namespace std;
 #include "sorting.h"
@@ -9,7 +15,7 @@ int main(int argc, char **argv) {
   int i, j, n = argc-2;
   int *a;
   if(argc < 3){
-        cerr << "Usage: " << argv[0] << " <sortType> <numbers...>" << endl;
+        cerr<<"Usage: "<<argv[0]<<" <sortType> <numbers...>"<< endl;
         return 1;
     }//the pointer will not point to NULL when there's only 1 argc ex. ./sort
 
@@ -22,12 +28,24 @@ int main(int argc, char **argv) {
 
   cout<<"Before: ";
   display(a,n);
-  if(sortType=="insert") insertSort(a,n);
+  if(sortType=="insertion") insertSort(a,n);
   else if(sortType=="bubble") bubbleSort(a,n);
-  else if(sortType=="select") selectSort(a,n);
+  else if(sortType=="selection") selectSort(a,n);
   else if(sortType=="heap") heapSort(a,n);
 
   display(a,n);
+  //function for calculating for each algorithm
+  /*vector<int> values(10000);
+    auto f = []() -> int { return rand() % 10000; };
+    generate(values.begin(), values.end(), f);
+
+    auto start = high_resolution_clock::now();
+    sort(values.begin(), values.end());
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;*/
   
   delete[]a;
   return 0; 
